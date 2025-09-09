7a98bf4b87d64e498c936ca71e86a0320948af5c
Amanuel <amanuelbehailu557@gmail.com>
2025-09-09 17:33:37 +0300

    bulkuser final

--- crm/schema.py
+++ crm/schema.py
@@ -1,12 +1,14 @@
 import graphene
 import re
-
+#universal database for customers to be stored
 Customers = []
-
+added= []
+temp_Storage = []
+#single customer
 class CreateCustomer(graphene.Mutation):
     class Arguments:
         name = graphene.String(required=True)
-        email = graphene.EmailField(required=True, unique = True)
+        email = graphene.String(required=True, unique = True)
         phone = graphene.String(required=False)
 
     def mutate(self, info, name, phone, email):
@@ -29,5 +31,32 @@ class CustomerInput(graphene.InputObjectType):
 class BulkCreateCustomer(graphene.Mutation):
     class Arguments:
         customers = graphene.List(CustomerInput, required=True)
-#list of the customerinputs
-#           
+
+    ok = graphene.Boolean()
+    #indicates if the mutation succeeded or failed
+    added_customers = graphene.List(CustomerInput)
+    #Returns the customers that were actually added with their fields
+    message = graphene.String()
+    #provides a human readable explanation of what happened
+
+    def mutate(self, info, customers):
+        temp_storage = []
+
+        for c in customers:
+            # validate phone if provided
+            if c.phone and not re.match(r"^(\+2519\d{8}|09\d{8})$", c.phone):
+                return BulkCreateCustomer(ok=False, added_customers=None, message=f"Invalid phone: {c.phone}")
+
+            # check duplicate email
+            if any(existing["email"] == c.email for existing in Customers):
+                return BulkCreateCustomer(ok=False, added_customers=None, message=f"Duplicate email: {c.email}")
+
+            temp_storage.append({"name": c.name, "email": c.email, "phone": c.phone})
+
+        # commit all
+        Customers.extend(temp_storage)
+        added_customers = [CustomerInput(**c) for c in temp_storage] #double * unpacks dictionary into keyword arguments, the ones put in temp_storage
+
+        return BulkCreateCustomer(ok=True, added_customers=added_customers, message = "All customers added successfuly")
+             
+          
