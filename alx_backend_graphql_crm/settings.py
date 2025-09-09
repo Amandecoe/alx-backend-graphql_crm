7546d33f005884ae30a7257ab49a52c31a1008ce
Amanuel <amanuelbehailu557@gmail.com>
2025-09-09 16:13:02 +0300

    settings edited

--- alx_backend_graphql_crm/settings.py
+++ alx_backend_graphql_crm/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    'crm',
 ]
 GRAPHENE = {
     'SCHEMA': 'alx_backend_graphql_crm.schema.schema'
