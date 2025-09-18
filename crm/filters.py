8e8aae0a2f43745be0ecc0dc04e983d9f22b7830
Amanuel <amanuelbehailu557@gmail.com>
2025-09-18 16:55:05 +0300

    OrderFilter implemented

--- crm/filters.py
+++ crm/filters.py
@@ -12,9 +12,15 @@ class Customerfilter(django_filters.Filterset):
         field_name = "created_at", lookup_Expr = "gte"
     )
     phone_startwith = django_filters.CharFilter(method = "phone_number_startswith")
+    
+    class meta:
+        model = Customer
+        Fields = ["name", "email", "created_at"]
 
     def phone_number_startswith(self, queryset, name, value):
-        return queryset.filter(phone_startwith = +1)     
+        return queryset.filter(phone_startwith = +1) 
+
+            
 
 class Productfilter(django_filters.Filterset):
     name_contains = django_filters.CharFilter(
@@ -28,5 +34,30 @@ class Productfilter(django_filters.Filterset):
     )
     low_stock_products = django_filters.NumberFilter (method = "product_with_low_Stock") #products with stock less than 10
     
+    class meta:
+        model = Product
+        Fields = ["name", "price", "stock"]
+
     def product_with_low_Stock(self, queryset, name, value):
         return queryset.filter(stock_lt = 10)
+
+class OrderFilter(django_filters.Filterset):
+    total_amount_contains = django_filters.RangeFilter(
+        field_name = total_amount, lookup_Expr = "gte"
+    )        
+    order_date_contains = django_filters.DateFilter(
+        field_name = order_date, lookup_Expr = "gte"
+    )
+    customer_name_contains = django_filters.CharFilter(
+        field_name = customer, lookup_Expr = "icontains"
+    )
+    products_name_contains = django.filters.CharFilter(
+        field_name = product, lookup_Expr = "icontains"
+    )
+    product_id = django.filter.NumberFilter(method = "filter_by_product")
+
+    def filter_by_product():
+        return queryset.Filter (products_id=2)
+    class meta:
+        model = Order
+        Fields = ["total_amount", "order_date", "customer"]
