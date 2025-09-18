f3b8b625fbbf7dea68af3698f0c9c0d8247b1733
Amanuel <amanuelbehailu557@gmail.com>
2025-09-18 15:42:26 +0300

    ProductFilter implemented

--- crm/filters.py
+++ crm/filters.py
@@ -1,5 +1,6 @@
 from .models import Customer, Product, Order
-
+import django_filters
+# icontains looks up for a field, i = case-insensetive and contains verifies if it contains
 class Customerfilter(django_filters.Filterset):
     name_contains = django_filters.CharFilter(
         field_name = "name", lookup_Expr = "icontains"
@@ -7,6 +8,25 @@ class Customerfilter(django_filters.Filterset):
     email_contains = django_filters.CharFilter(
         field_name = "email", lookup_Expr = "icontains"
     )
-    created_at = django_filters.DateFilter(
+    created_at_contains = django_filters.DateFilter(
         field_name = "created_at", lookup_Expr = "gte"
     )
+    phone_startwith = django_filters.CharFilter(method = "phone_number_startswith")
+
+    def phone_number_startswith(self, queryset, name, value):
+        return queryset.filter(phone_startwith = +1)     
+
+class Productfilter(django_filters.Filterset):
+    name_contains = django_filters.CharFilter(
+        field_name = "name", lookup_Expr = "icontains"
+    )       
+    price_contains = django_filters.RangeFilter(
+        field_name = "price", lookup_Expr = "gte"  #gte stands for greater than or equal to
+    )
+    stock_contains = django_filters.RangeFilter(
+        field_name = "stock", lookup_Expr = "gte"
+    )
+    low_stock_products = django_filters.NumberFilter (method = "product_with_low_Stock") #products with stock less than 10
+    
+    def product_with_low_Stock(self, queryset, name, value):
+        return queryset.filter(stock_lt = 10)
